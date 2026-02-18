7dac6b3f67dd0b66aced621321d0432196bed6d7
code-manush <manushpatel1426@gmail.com>
2026-02-19 00:54:33 +0530

    Remove unused imports from App component and template changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Cert from './components/cert'
-import CertPage from './components/certpage'
 import CertificateVerification from './components/CertificateVerification'
 
 
